0dda1d28efec2843b2b0acdd9a9bc99f8a0b62d6
venkat-krish <venkat.kanithan@gmail.com>
2019-09-04 11:18:43 +0530

    Comments added on the module

--- mobile_manufacturing.py
+++ mobile_manufacturing.py
@@ -1,10 +1,24 @@
+##################################################################################################
+# Assignment 2: Problem PS1
+# Group #: AS2_CH1_B1_G6
+# Members: Venkataramanan K, Bala Kavin Pon, Balavijay D
+# Problem Statement: 
+#   Mobile manufacturing tasks should be scheduled in an optimized manner such that the idle time of 
+#   assembly unit should be in minimal time.
+##################################################################################################
+
 import logging
 import os
 
+# Instantiation of logger object
 logger = logging.getLogger(__name__)
 
 """
     Task class is a data structure for the mobile manufacture tasks.
+    Attributes:
+        Task Id
+        Manufacture time
+        Assemble Time
 """
 class Task():
 
@@ -35,17 +49,17 @@ class ManufactureMobile():
             logger.error("Input file is not found. Please provide valid input file.\t<{0}>".format(fne))
 
     def __fetch_tasks(self, input_file):
-        task_arr = []
+        task_task_list = []
         try:
             with open(input_file, 'r+') as tfp:
-                task_arr =  [self.__map_task(line) for line in tfp.readlines()]
+                task_task_list =  [self.__map_task(line) for line in tfp.readlines()]
                 tfp.close() # Close the file handler
         except ValueError as ve:
             logger.error("Invalid input data please correct your input and re-run the program.\t<{0}>".format(ve))
         except FileNotFoundError as fne:
             raise FileNotFoundError(fne)
         
-        return task_arr
+        return task_task_list
 
     def __map_task(self, task_info):
         try:
@@ -60,12 +74,12 @@ class ManufactureMobile():
         it uses the merge sort algorithm. 
         the complexity of the runtime for this function is O(nlogn).
     """
-    def merge_sort_tasks(self, arr):
-        if len(arr) > 1: # Return condition: Checking the size of the array should be more than 1
-            mid = len(arr)//2
-            left = arr[:mid]
-            right = arr[mid:]
-            # Recursive call with left and right split of array
+    def merge_sort_tasks(self, task_list):
+        if len(task_list) > 1: # Return condition: Checking the size of the task_listay should be more than 1
+            mid = len(task_list)//2
+            left = task_list[:mid]
+            right = task_list[mid:]
+            # Recursive call with left and right split of task_listay
             self.merge_sort_tasks(left)
             self.merge_sort_tasks(right)
             
@@ -75,64 +89,69 @@ class ManufactureMobile():
                 # whenever left element manufacture time is lesser than the right element manufacture time then 
                 # add the left item first
                 if left[i].manufacture_time < right[j].manufacture_time: 
-                    arr[k] = left[i]
+                    task_list[k] = left[i]
                     i+=1
                 elif left[i].manufacture_time == right[j].manufacture_time: # When the left & right elements manufacture time are same then go for assemble time
                     if left[i].assemble_time < right[j].assemble_time: # if right assemble time is larger than the left then move the right element.
-                        arr[k] = right[j]
+                        task_list[k] = right[j]
                         j+=1
                     else:
-                        arr[k] = left[i]
+                        task_list[k] = left[i]
                         i+=1
                 else: # When right elements manufacture time is larger
-                    arr[k] = right[j]
+                    task_list[k] = right[j]
                     j+=1
                 k+=1
 
             # Left iteration happens only when the index is still lesser than the length of left split.
             while i < len(left):
-                arr[k] = left[i]
+                task_list[k] = left[i]
                 i += 1
                 k += 1
             # right iteration happens only when the index is still lesser than the length of right split.
             while j < len(right):
-                arr[k] = right[j]
+                task_list[k] = right[j]
                 j += 1
                 k += 1
     
     
     """"
-        Schedule function does the scheduling activity of the tasks given 
+        schedule() - Schedule function does the scheduling activity of the tasks given 
     """
     def schedule(self):
-        # 1. sort the tasks in based on the total production time in increasing order
+        # 1. Copy of the input task list
         task_list = self.tasks[:]
-        # Invoking merge sort function to sort the tasks
+        # 2. Invoking merge sort function to sort the tasks
         self.merge_sort_tasks(task_list)
         logger.debug("Sorted jobs {0}".format(task_list))
-        self.write_result(self.__get_runtime_idle_ass(task_list))
+        # 3. Invocation of runtime assembly unit idle time
+        self.write_result(self.__get_runtime_assembly_idle(task_list))
 
     """
         Extract Total run time and wait time in units 
     """
-    def __get_runtime_idle_ass(self,arr):
-        run_time=0
-        idle_time=0
-        ass_time=0
-        lag = 0
+    def __get_runtime_assembly_idle(self,task_list):
+        run_time = assemble_time = idle_time = lag = 0
         job_sequence = list()
-        for i in range(0, len(arr)):
-            job_sequence.append(arr[i].task_id)
-            run_time += arr[i].manufacture_time  # manufacturing unit runs in sequence also its current scale for run time
-            diff = run_time - (idle_time + ass_time)  # compute the difference in assembly time of previous values
-            if( diff > 0):                            #  +ve means add to idle time
-                idle_time += diff                     #  -ve means its a carry forward lagg
+        
+        for i in range(0, len(task_list)):
+            # Add the optimal jobs in the sequence of running.
+            job_sequence.append(task_list[i].task_id)
+            # Running time indicates the sum of parts manufacture time.
+            run_time += task_list[i].manufacture_time  # manufacturing unit runs in sequence also its current scale for run time
+            # Time difference is runtime - (idle time and assemble time)
+            time_diff = run_time - (idle_time + assemble_time)  # compute the time_difference in assembly time of previous values
+        
+            if (time_diff > 0):                            #  +ve means add to idle time
+                idle_time += time_diff                     #  -ve means its a task_list forward lag
                 lag = 0
             else:
-                lag = -diff
-            ass_time += arr[i].assemble_time
+                lag = -time_diff
+            # Take the sum of assembly time
+            assemble_time += task_list[i].assemble_time
 
-        total_time = run_time + lag + arr[len(arr)-1].assemble_time
+        # Overall production time includes runtime + lag time + last item's assemble time
+        total_time = run_time + lag + task_list[len(task_list)-1].assemble_time
         logger.debug("Sequence list: {0}, Idle : {1}, Prod Time: {2}".format(job_sequence, idle_time, total_time))
 
         return (job_sequence, total_time, idle_time)
